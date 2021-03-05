5f2c5f250982e936f407f2834707fd0a201b99da
sol <wonderfulpine@gmail.com>
2021-03-05 14:04:45 +0900

    국내주식, 해외주식, 코인의 종목명, 종목 코드 수집

--- app/tests.py
+++ app/tests.py
@@ -6,3 +6,20 @@ Copyright (c) 2019 - present AppSeed.us
 from django.test import TestCase
 
 # Create your tests here.
+from pandas_datareader import data
+import pandas as pd
+import FinanceDataReader as fdr
+import pyupbit
+
+# 국내주식
+df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
+# 해외주식
+df_nasdaq = fdr.StockListing('NASDAQ')
+# 코인
+
+
+tickers = pyupbit.get_tickers()
+print(tickers)
+# 원화 시장의 코인 리스트
+t = pyupbit.get_tickers(fiat="KRW")
+print(t)
